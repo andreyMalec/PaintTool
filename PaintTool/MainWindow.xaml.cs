d1c6a05d5084aeb293853d57da116f3b1be9ee51
Malez228 <andrey193728@yandex.ru>
2017-07-02 18:00:35 +0300

    Вроде весь интерфейс переписал

---
+++ PaintTool/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PaintTool
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("PaintTool"+ Environment.NewLine +
+            Environment.NewLine+"Version: "+ System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString()+
+            Environment.NewLine + "Dev: Andrey Malec"+
+            Environment.NewLine + "Copyright ©  2017"+new string(' ', 64),
+            "About PaintTool", MessageBoxButton.OK);
+        }
+    }
+}
